e7ce297fff751880252e4a29d2cdd3db001179a0
nakajima-ittoku <ittoku.nakajima@ysinc.co.jp>
2024-01-30 20:32:55 +0900

    chore: json-serverを使う実装に変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+
+// TODO: pathが異なる場合があります。適宜修正してください。
+import { InputForm } from "./components/InputForm";
+import { List } from "./components/List";
+
+const StyledContainer = styled.div`
+  width: 327px;
+  margin: 24px auto;
+`;
+
+export type TodoList = {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+}
+
 function App() {
+  const [todoList, setTodoList] = React.useState<TodoList[]>([]);
+
+  const getTodoList = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      if(!res.ok) {
+        throw new Error();
+      }
+
+      const data = await res.json();
+      setTodoList(data);
+    } catch (error) {
+      alert('「npm run json-server」でjson-serverを起動してください。')
+    }
+  }
+
+  React.useEffect(() => {
+    getTodoList();
+  }, []);
 
   return (
-    <div>
-      <p>Todoアプリを利用してGitの練習をしていきます。</p>
-    </div>
+    <StyledContainer>
+      <Typography variant="h1" sx={{fontSize: "32px"}}>今日やること</Typography>
+      <InputForm getData={getTodoList} />
+      <List getData={getTodoList} todoList={todoList} />
+    </StyledContainer>
   )
 }
 
-export default App
+export default App;
