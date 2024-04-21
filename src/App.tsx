80e08f348dd156d2115654c4d39b1acaa13a72a2
Megumi Kaburagi <ys.training230526@gmail.com>
2024-04-21 17:03:55 +0900

    chore: Git練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+
+import { InputForm } from "./components/InputForm";
+import { List } from "./components/List";
+
+const StyledContainer = styled.div`
+  width: 327px;
+  margin: 24px auto;
+`;
+
+export type TodoList = {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+};
+
 function App() {
+  const [todoList, setTodoList] = React.useState<TodoList[]>([]);
 
   return (
-    <div>
-      <p>Todoアプリを利用してGitの練習をしていきます。</p>
-    </div>
-  )
+    <StyledContainer>
+      <Typography variant="h1" sx={{ fontSize: "32px" }}>
+        今日やること
+      </Typography>
+      <InputForm setTodoList={setTodoList} todoList={todoList} />
+      <List setTodoList={setTodoList} todoList={todoList} />
+    </StyledContainer>
+  );
 }
 
-export default App
+export default App;
